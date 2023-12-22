f3485e77a7521df639b3f0dcacd6f3c4db6b4c02
Himanshu Koshti <Himfeedback74@gmail.com>
2023-12-22 23:55:13 +0530

    working logback with custom logfile

---
+++ src/main/java/com/example/demo/two.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class two {
+    static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+    void run(){
+        logger.info("***************otherclasee");
+
+    }
+
+
+}
