9978de51c976d22ba97a0af983729b14ac11836e
Himanshu Koshti <Himfeedback74@gmail.com>
2024-01-28 23:20:03 +0530

    Add profile

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 
 @SpringBootApplication
 public class DemoApplication {
@@ -13,10 +17,16 @@ public class DemoApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(DemoApplication.class, args);
-        logger.info("Hello from main");
-
+        logger.info(Arrays.toString("ab".getBytes()));
+        logger.info(Arrays.toString("b".getBytes()));
+        logger.info(Arrays.toString("c".getBytes()));
+        logger.info(Arrays.toString("d".getBytes()));
+        logger.info(Arrays.toString("e".getBytes()));
+        List<String> names = Arrays.asList("Alice", "Bob", "David", "Charlie");
+        System.out.println(names);
+        Collections.sort(names, String::compareTo);
+        System.out.println(names);
     }
-
 }
 
 
--- src/main/java/com/example/demo/customLog.java
+++ src/main/java/com/example/demo/customLog.java
@@ -4,19 +4,21 @@ import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
 import ch.qos.logback.core.joran.spi.JoranException;
 import org.apache.commons.io.IOUtils;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.DefaultResourceLoader;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 @Component
+@Profile("dev")
 public class customLog {
+    Logger logger = LoggerFactory.getLogger("custom log");
     @Bean
     private void setup() throws IOException {
         ResourceLoader resourceLoader;
@@ -29,10 +31,15 @@ public class customLog {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
+
         tempFile.deleteOnExit();
+        System.out.println(tempFile.length());
         try (FileOutputStream out = new FileOutputStream(tempFile)) {
             IOUtils.copy(resource.getInputStream(), out);
         }
+        System.out.println(tempFile);
+
         JoranConfigurator configurator = new JoranConfigurator();
         LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
         configurator.setContext(context);
@@ -40,6 +47,16 @@ public class customLog {
         try {
             configurator.doConfigure(tempFile);
         } catch (JoranException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+            String line;
+//            System.out.println(reader.readLine());
+            while ((line = reader.readLine()) != null) {
+                logger.info(line);
+            }
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
